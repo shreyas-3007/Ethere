9a8d5fdffe5d064c364f6ab265a9830be3f1298f
saurabh bagal <bagalsaurabh644@gmail.com>
2024-04-20 23:31:07 +0530

    profile page pop up view

---
+++ script/profilepage.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const profileIcon = document.querySelector('.profile-icon');
+  const profileModal = document.getElementById('profileModal');
+  const closeModalBtn = document.getElementById('closeModal');
+  const clickevent= document.getElementById('.profile-icon');
+
+  profileIcon.addEventListener('click', function() {
+      profileModal.style.display = 'block';
+  });
+
+  closeModalBtn.addEventListener('click', function() {
+      profileModal.style.display = 'none';
+  });
+});
+
+// const profileIcon = document.getElementById('profile-icon');
+// const sidebar = document.getElementById('profileModal');
+
+// profileIcon.addEventListener('click', () => {
+//     sidebar.classList.toggle('active');
+// });
+
+
