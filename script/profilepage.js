6d5797b493f4098e2b38706105fb74e4ea201cad
saurabh bagal <bagalsaurabh644@gmail.com>
2024-04-23 16:50:00 +0530

    nodemon script added

--- script/profilepage.js
+++ script/profilepage.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const profileIcon = document.querySelector('.profile-icon');
+  const profileIcon = document.querySelector('.action_container');
   const profileModal = document.getElementById('profileModal');
   const closeModalBtn = document.getElementById('closeModal');
   const clickevent= document.getElementById('.profile-icon');
